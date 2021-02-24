1ded8e7e9ac28c4b6f4152f8a4bf3f1707aff3de
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-02-24 09:33:27 +0900

    feat: user iconの拡張子をjpeg,png,gifに制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
+  validate :icon_content_type, if: :was_attached?
+
+  def icon_content_type
+    extension = ['image/png', 'image/jpeg', 'image/gif']
+    errors.add(:icon, 'の拡張子が間違っています') unless icon.content_type.in?(extension)
+  end
+
+  def was_attached?
+    icon.attached?
+  end
 
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
