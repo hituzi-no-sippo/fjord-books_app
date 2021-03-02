0c909c54616841121c033bbe69ed2531be96d3d3
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-03 08:04:19 +0900

    refactor: db処理をredirect_to内に入れないように変更

--- app/controllers/users/followings_controller.rb
+++ app/controllers/users/followings_controller.rb
@@ -8,30 +8,28 @@ class Users::FollowingsController < ApplicationController
   def create
     following = current_user.following_relationships.build(following_id: params[:id])
 
-    redirect_to(
-      user_path(params[:id]),
-      if following.save
-        { notice: t('controllers.common.notice_success', event: t('followings.follow')) }
-      elsif following.errors.full_messages.include?(t('errors.format', attribute: 'Following', message: t('errors.messages.taken')))
-        { notice: t('followings.already_followed') }
-      else
-        { alert: following.errors.full_messages }
-      end
-    )
+    message = if following.save
+                { notice: t('controllers.common.notice_success', event: t('followings.follow')) }
+              elsif following.errors.full_messages.include?(t('errors.format', attribute: 'Following', message: t('errors.messages.taken')))
+                { notice: t('followings.already_followed') }
+              else
+                { alert: following.errors.full_messages }
+              end
+
+    redirect_to(user_path(params[:id]), message)
   end
 
   def destroy
     follow = current_user.following_relationships.find_by(following_id: params[:id])
 
-    redirect_to(
-      user_path(params[:id]),
-      if follow.nil?
-        { notice: t('followings.not_following') }
-      elsif follow.destroy
-        { notice: t('controllers.common.notice_success', event: t('followings.unfollow')) }
-      else
-        { alert: following.errors.full_messages }
-      end
-    )
+    message = if follow.nil?
+                { notice: t('followings.not_following') }
+              elsif follow.destroy
+                { notice: t('controllers.common.notice_success', event: t('followings.unfollow')) }
+              else
+                { alert: following.errors.full_messages }
+              end
+
+    redirect_to(user_path(params[:id]), message)
   end
 end
