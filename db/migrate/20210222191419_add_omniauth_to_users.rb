b6b52c7975733a89859124223b7b8731e242a8bf
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-04 07:06:48 +0900

    fix: `db:rollback` ができない問題を修正

--- db/migrate/20210222191419_add_omniauth_to_users.rb
+++ db/migrate/20210222191419_add_omniauth_to_users.rb
@@ -4,7 +4,7 @@ class AddOmniauthToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :provider, :string, null: false, default: ''
     add_column :users, :uid,      :string, null: false, default: ''
-  end
 
-  add_index :users, %i[uid provider], unique: true
+    add_index :users, %i[uid provider], unique: true
+  end
 end
