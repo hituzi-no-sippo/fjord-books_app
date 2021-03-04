7161fc51240c6a8f2fbef76271f84a762c172b19
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-04 16:41:57 +0900

    test: bookのsystem testをi18n対応する

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -10,39 +10,39 @@ class BooksTest < ApplicationSystemTestCase
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: Book.model_name.human
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
+    click_on I18n.t('views.common.new')
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in Book.human_attribute_name(:memo), with: @book.memo
+    fill_in Book.human_attribute_name(:title), with: @book.title
+    click_on I18n.t('helpers.submit.create')
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_create', name: Book.model_name.human)
+    click_on I18n.t('views.common.back')
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
+    click_on I18n.t('views.common.edit'), match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in Book.human_attribute_name(:memo), with: @book.memo
+    fill_in Book.human_attribute_name(:title), with: @book.title
+    click_on I18n.t('helpers.submit.update')
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_update', name: Book.model_name.human)
+    click_on I18n.t('views.common.back')
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on I18n.t('views.common.destroy'), match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text I18n.t('controllers.common.notice_destroy', name: Book.model_name.human)
   end
 end
