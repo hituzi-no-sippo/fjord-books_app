aaca86dc281c76fc0a5f88a43381d536f78576d4
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-05 14:52:43 +0900

    test: Report#editable? のtestを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    owner = User.new(email: 'owner@example.com')
+    report = owner.reports.new(title: 'test title', content: 'test content')
+
+    assert report.editable?(owner)
+
+    non_owner = User.new(email: 'non_owner@example.com')
+
+    assert_not report.editable?(non_owner)
+  end
 end
