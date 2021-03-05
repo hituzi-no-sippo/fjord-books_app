2e8982b13572cc5f4bd7dfc549b7d842e64e32fa
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-05 16:05:34 +0900

    test: User#follow のtestを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,19 @@
 require 'test_helper'
 
 class UsersTest < ActiveSupport::TestCase
+  def make_two_user
+    [User.create!(email: 'one@example.com', password: '123456'),
+     User.create!(email: 'two@example.com', password: '123456')]
+  end
+
+  test '#follow' do
+    one, two = make_two_user
+
+    assert_not one.following?(two)
+    one.follow(two)
+    assert one.following?(two)
+  end
+
   test '#name_or_email' do
     email = 'alice@example.com'
     user = User.new(email: email)
